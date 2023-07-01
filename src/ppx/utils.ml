c289823e4ada026e0aad3665e2581c40137829d6
Woonki Moon <woonki.moon@gmail.com>
2023-07-01 16:11:07 +0900

    remove metaquot

--- src/ppx/utils.ml
+++ src/ppx/utils.ml
@@ -131,4 +131,5 @@ let expr_func ?(loc = Location.none) ~arity e =
   in
   Exp.construct ~attrs:[ attr_arity ] { txt = Lident "Function$"; loc } (Some e)
 
-let ctyp_json_t = Typ.constr (mknoloc [%lid Js.Json.t]) []
+let ctyp_json_t =
+  Typ.constr (mknoloc (Ldot (Ldot (Lident "Js", "Json"), "t"))) []
